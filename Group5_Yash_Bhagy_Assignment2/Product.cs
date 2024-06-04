63b8bb3a8554491702e0d6538e78639ad239e3ec
Yash Patel <Ypatel9094@conestogac.on.ca>
2024-06-03 20:37:51 -0400

    Created product test class & installed packages.

--- Group5_Yash_Bhagy_Assignment2/Product.cs
+++ Group5_Yash_Bhagy_Assignment2/Product.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Group5_Yash_Bhagy_Assignment2
 {
-    internal class Product
+    public class Product
     {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+        public Product(int productId, string productName, decimal price, int stock)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        public void IncreaseStockValue(int price)
+        {
+            if (price > 0)
+            {
+                Stock += price;
+            }
+        }
+
+        public void DecreaseStockValue(int price)
+        {
+            if (price > 0 && price <= Stock)
+            {
+                Stock -= price;
+            }
+        }
     }
 }
